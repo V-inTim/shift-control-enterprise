19787b11f701d23927fda9ad1333aa5ea3371365
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2025-03-26 00:17:42 +0300

    Add dependencies for db and migrations.

--- src/main/java/com/example/shift_control_enterprise/ShiftControlEnterpriseApplication.java
+++ src/main/java/com/example/shift_control_enterprise/ShiftControlEnterpriseApplication.java
@@ -2,8 +2,10 @@ package com.example.shift_control_enterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/.env.properties")
 public class ShiftControlEnterpriseApplication {
 
 	public static void main(String[] args) {
