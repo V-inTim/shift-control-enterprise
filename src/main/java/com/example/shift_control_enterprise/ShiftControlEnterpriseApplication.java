3176328db51a0c96a2a42d52376e59be0fb03a18
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2025-03-25 20:00:09 +0300

    Create structure.

---
+++ src/main/java/com/example/shift_control_enterprise/ShiftControlEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package com.example.shift_control_enterprise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftControlEnterpriseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftControlEnterpriseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shift_control_enterprise/ShiftControlEnterpriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shift_control_enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftControlEnterpriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
