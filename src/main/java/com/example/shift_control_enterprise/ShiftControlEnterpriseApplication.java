e382f7878fa618f28ec936763a31b39b7d61f8ab
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2025-03-26 00:37:56 +0300

    Fix. Add config for .env.properties and remove @PropertySource.

--- src/main/java/com/example/shift_control_enterprise/ShiftControlEnterpriseApplication.java
+++ src/main/java/com/example/shift_control_enterprise/ShiftControlEnterpriseApplication.java
@@ -2,10 +2,8 @@ package com.example.shift_control_enterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("file:${user.dir}/.env.properties")
 public class ShiftControlEnterpriseApplication {
 
 	public static void main(String[] args) {
