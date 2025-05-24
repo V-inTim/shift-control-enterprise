e538a2e2e952753b21d3f41498c388cb2082a7d9
V-inTim <vinichuk.timofey@mail.ru>
2025-05-24 19:15:23 +0300

    Add Gender column to Employee.

--- src/main/java/com/example/shift_control_enterprise/dto/EmployeeDto.java
+++ src/main/java/com/example/shift_control_enterprise/dto/EmployeeDto.java
@@ -1,5 +1,8 @@
 package com.example.shift_control_enterprise.dto;
 
+import com.example.shift_control_enterprise.type.Gender;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,5 +17,8 @@ public class EmployeeDto {
     @NotNull
     private String lastName;
     private String secondName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate dateOfBirth;
+    @NotNull
+    private Gender gender;
 }
