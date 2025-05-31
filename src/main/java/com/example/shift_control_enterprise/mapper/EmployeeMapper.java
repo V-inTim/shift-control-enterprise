89d45b332e1c350caa1edb8ec70013f09012f7a1
V-inTim <vinichuk.timofey@mail.ru>
2025-05-31 16:33:47 +0300

    Change employee Mapper and Repository.

--- src/main/java/com/example/shift_control_enterprise/mapper/EmployeeMapper.java
+++ src/main/java/com/example/shift_control_enterprise/mapper/EmployeeMapper.java
@@ -1,6 +1,8 @@
 package com.example.shift_control_enterprise.mapper;
 
 import com.example.shift_control_enterprise.dto.EmployeeDto;
+import com.example.shift_control_enterprise.dto.WorkShiftsPerPeriodDto;
+import com.example.shift_control_enterprise.dto.WorkShiftsSumPerPeriodDto;
 import com.example.shift_control_enterprise.entity.Employee;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,4 +11,8 @@ import org.mapstruct.Mapping;
 public interface EmployeeMapper {
     @Mapping(source = "dto.gender", target = "gender")
     Employee dtoToEmployee(EmployeeDto dto);
+
+
+    WorkShiftsPerPeriodDto employeeToWorkShiftsPerWeekDto(Employee employee);
+    WorkShiftsSumPerPeriodDto employeeToWorkShiftsSumPerPeriodDto(Employee employee);
 }
--- src/main/java/com/example/shift_control_enterprise/repository/EmployeeRepository.java
+++ src/main/java/com/example/shift_control_enterprise/repository/EmployeeRepository.java
@@ -1,12 +1,14 @@
 package com.example.shift_control_enterprise.repository;
 
 import com.example.shift_control_enterprise.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findAllByEnterpriseId(Long enterpriseId);
+    Page<Employee> findAllByEnterpriseId(Long enterpriseId, Pageable pageable);
     boolean existsByIdAndEnterpriseId(Long id, Long enterpriseId);
 }
