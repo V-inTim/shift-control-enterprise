c470605ee33bcfbacd4c29066d6833d6b13f2389
V-inTim <vinichuk.timofey@mail.ru>
2025-05-31 14:47:38 +0300

    Add getEnterpriseId method to AuthUtils.

--- src/main/java/com/example/shift_control_enterprise/security/AuthUtils.java
+++ src/main/java/com/example/shift_control_enterprise/security/AuthUtils.java
@@ -1,5 +1,6 @@
 package com.example.shift_control_enterprise.security;
 
+import com.example.shift_control_enterprise.exception.EnterpriseException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,5 +17,17 @@ public class AuthUtils {
             throw new AccessDeniedException("Unauthorized or invalid JWT token");
         }
         return  UUID.fromString(jwt.getClaim("user_id"));
+    }
+    public Long getEnterpriseId() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !(authentication.getPrincipal() instanceof Jwt jwt)) {
+            throw new AccessDeniedException("Неавторизованный и инвалидный JWT token");
+        }
+        try {
+            String enterpriseIdClaim = jwt.getClaim("enterprise_id").toString();
+            return Long.valueOf(enterpriseIdClaim);
+        } catch (NumberFormatException e) {
+            throw new EnterpriseException("Инвалидный enterprise_id в JWT token");
+        }
     }
 }
